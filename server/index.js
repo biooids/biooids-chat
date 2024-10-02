141438d4307af1dac7a4bbbd11894fe31055454a
biooids <biooidsbiooid@gmail.com>
2024-10-02 06:02:43 +0200

    "Added dependencies, configured MongoDB connection, and implemented error handling in server/index.js"

--- server/index.js
+++ server/index.js
@@ -1,7 +1,38 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+import authRoutes from "../server/routes/auth.routes.js";
+
+dotenv.config();
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB Atlas connected");
+  })
+  .catch((error) => {
+    console.log(
+      "MongoDB Atlas failed to connect due to error :",
+      error.message || error
+    );
+  });
+
+app.use("/api/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  const message =
+    `action failed due to : ${error.message}` ||
+    "action failed due to : internal server error";
+  res.status(statusCode).json({ success: false, statusCode, message });
+});
 
 const server = app.listen(PORT, () => {
   const host = server.address().address;
---
+++ server/routes/auth.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {
+  //   forgotPassword,
+  //   logIn,
+  signUp,
+} from "../controllers/auth.controllers.js";
+
+const router = express.Router();
+
+router.post("/sign-up", signUp);
+// router.post("/login", logIn);
+// router.post("/forgotpassword", forgotPassword);
+
+export default router;
